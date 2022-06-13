049c0e396945a12753f37bc047a3ecd8ac7ebdfd
Aleksei_V <user008.vel@gmail.com>
2022-06-13 15:52:11 +0300

    Ex1 done

--- Examples/Example003_Sum/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿int numberA = new Random().Next(1, 100); // 1 2 3 4 ... 99
-Console.WriteLine(numberA);
-int numberB = new Random().Next(1, 100); // 1 2 3 4 ... 99
-Console.WriteLine(numberB);
-int result = numberA + numberB;
-Console.WriteLine(result);
--- Examples/Example004_Div/Program.cs
+++
@@ -1,3 +0,0 @@
-﻿double numberA = 9;
-double numberB = 4;
-Console.WriteLine(numberA / numberB);
